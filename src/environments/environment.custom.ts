5c6d1beccb9b2b94cadddb672582362017e26df2
Linus <linus@wieland.lu>
2022-07-02 15:59:04 +0200

    test gitignore

--- src/environments/environment.custom.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-   production: false,
-   NODE_VERSION: "undefined",
-   SITE_ID: "7da29450-d489-4c59-9c2e-61d0cc5f52f0",
-   API_KEY_NETLIFY: "H90qZW5PFpppTk0Cby1FJSBIOfzhOrAlbt61CSe5d_8",
-   URL: "https://new-schlosswochen.netlify.app",
-};
