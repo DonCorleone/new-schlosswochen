305195ef7470ab9da712be92726e0835d6e0a6de
Linus <linus@wieland.lu>
2022-07-08 07:49:28 +0200

    w/o markers

--- src/app/schlosswochen/components/main-content/maps/maps.component.ts
+++ src/app/schlosswochen/components/main-content/maps/maps.component.ts
@@ -27,14 +27,14 @@ export class MapsComponent implements OnInit {
   markers: any[] = [];
 
   ngOnInit(): void {
-    this.markers.push({
+/*    this.markers.push({
       position: {
         lat: 47.0417285,
         lng: 8.3260751,
       },
       title: 'Marker title ' + (this.markers.length + 1),
       options: { animation: google.maps.Animation.DROP },
-    });
+    });*/
   }
   zoomIn() {
     if (!this.options || !this.options.maxZoom) {
