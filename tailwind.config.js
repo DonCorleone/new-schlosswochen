cdbb35ba0ae9e237a82ce78c68f1cdcb107db391
Linus <linus@wieland.lu>
2022-07-11 23:50:12 +0200

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bleu-ceruleen-pale': '#c6d5cc',
+        'terre-ombre-brule': '#4c423d',
+        'vert-59' : '#428f70',
+        'rouge-rubia' : '#943a4d'
+      },
+    }
   },
   plugins: [
     require('@tailwindcss/typography')
