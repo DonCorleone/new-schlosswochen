ebacb64d7d80859de8e83e3b81e86a97b6869630
Linus <linus@wieland.lu>
2022-08-17 21:24:25 +0200

    download

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -12,6 +12,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import {MatExpansionModule} from "@angular/material/expansion";
+import {MatChipsModule} from "@angular/material/chips";
 
 @NgModule({
   declarations: [],
@@ -29,6 +30,7 @@ import {MatExpansionModule} from "@angular/material/expansion";
     MatFormFieldModule,
     MatNativeDateModule,
     MatExpansionModule,
+    MatChipsModule
   ],
 })
 export class SharedModule {}
